ab3ec1fcda52a39305af29614d877b5924e12e14
aartuuroo20 <arturequejo@gmail.com>
2023-02-26 12:29:55 +0100

    Updates

--- src/bmicalculator/BMICalculatorClient.java
+++ src/bmicalculator/BMICalculatorClient.java
@@ -10,6 +10,7 @@ import org.omg.CosNaming.*;
 import org.omg.CosNaming.NamingContextPackage.*;
 import org.omg.CORBA.*;
 
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -49,12 +50,49 @@ public class BMICalculatorClient {
             System.out.println("Obtained BMICalculator object: " + bmicalcImpl);
             System.out.println("BMI: " + bmicalcImpl.calculateBMI(height, weight));
 
-            if(gender == "female"){
-                System.out.println("TMB: " + bmicalcImpl.calculateTMBf(height, weight, age));
-            }else{
-                System.out.println("TMB: " + bmicalcImpl.calculateTMB(height, weight, age));
+            HashMap<String, Integer> listaComidasSubirPeso = new HashMap<>();
+            listaComidasSubirPeso.put("Primera comida", 1000);
+            listaComidasSubirPeso.put("Segunda comida", 1200);
+            listaComidasSubirPeso.put("Tercera comida", 1000);
+            listaComidasSubirPeso.put("Cuarta comida", 1200);
+            int totalCalorias = 0;
+            for (int calorias : listaComidasSubirPeso.values()) {
+                totalCalorias += calorias;
             }
 
+            HashMap<String, Integer> listaComidasBajarPeso = new HashMap<>();
+            listaComidasBajarPeso.put("Primera comida", 500);
+            listaComidasBajarPeso.put("Segunda comida", 500);
+            listaComidasBajarPeso.put("Tercera comida", 500);
+            int totalCaloriasBajarPeso = 0;
+            for (int calorias : listaComidasBajarPeso.values()) {
+                totalCaloriasBajarPeso += calorias;
+            }
+
+            if(gender == "female"){
+                double tmb = bmicalcImpl.calculateTMBf(height, weight, age);
+                System.out.println("Your TMB is: " + tmb);
+
+                System.out.println("If you want to gain weight, you need to eat more,  an example of what you should eat is: ");
+                System.out.println(listaComidasSubirPeso.keySet() + " " + listaComidasSubirPeso.values() + " calories");
+                
+                System.out.println("If you want to lose weight, you need to eat less,  an example of what you should eat is: ");
+                System.out.println(listaComidasSubirPeso.keySet() + " " + listaComidasSubirPeso.values() + " calories");
+              
+                System.out.println("If you want to mantain weight, you are eating the right amount of calories.");
+                
+            }else{
+                double tmb = bmicalcImpl.calculateTMB(height, weight, age);
+                System.out.println("Your TMB is: " + tmb);
+
+                System.out.println("If you want to gain weight, you need to eat more,  an example of what you should eat is: ");
+                System.out.println(listaComidasSubirPeso.keySet() + " " + listaComidasSubirPeso.values() + " calories, " + "total calories: " + totalCalorias + " calories");
+                
+                System.out.println("If you want to lose weight, you need to eat less,  an example of what you should eat is: ");
+                System.out.println(listaComidasSubirPeso.keySet() + " " + listaComidasSubirPeso.values() + " calories, " + "total calories: " + totalCaloriasBajarPeso + " calories");
+              
+                System.out.println("If you want to mantain weight, you are eating the right amount of calories.");
+            }
 
             bmicalcImpl.shutdown();
         } catch (org.omg.CORBA.ORBPackage.InvalidName | NotFound | CannotProceed | InvalidName ex) {
